e0e9181d3178fa81541ce7d7e27906c479e11533
Mohammad Hossein Motallebi <mhmotallebi@yahoo.com>
2016-11-23 03:54:39 -0700

    added definition of SimpleFeatureComputer + migrated to Eclipse + changed Time to Date

--- src/main/java/me/motallebi/hashtagger/FeatureComputer.java
+++ src/main/java/me/motallebi/hashtagger/FeatureComputer.java
@@ -3,7 +3,6 @@
  */
 package me.motallebi.hashtagger;
 
-import java.io.OutputStream;
 import java.util.Date;
 
 /**
@@ -12,37 +11,23 @@ import java.util.Date;
  */
 public interface FeatureComputer {
 
-	public Float LS(NewsArticle a, Hashtag h, Time LAMBDA ); // Local Simiarity
+	public Float LS(NewsArticle a, Hashtag h, Date LAMBDA ); // Local Simiarity
 	
-	public Float LF(NewsArticle a, Hashtag h, Time LAMBDA ); // Local Frequency
+	public Float LF(NewsArticle a, Hashtag h, Date LAMBDA ); // Local Frequency
 	
-	public Float GS(NewsArticle a, Hashtag h, Time GAMMA ); // Global Similarity
+	public Float GS(NewsArticle a, Hashtag h, Date GAMMA ); // Global Similarity
 	
-	public Float GF(Hashtag h, Time GAMMA ); // Global Frequency
+	public Float GF(Hashtag h, Date GAMMA ); // Global Frequency
 	
-	public Float TR(NewsArticle a, Hashtag h, Time t); // TRending hashtags
+	public Float TR(NewsArticle a, Hashtag h, Date t); // TRending hashtags
 	/* needs a function tell how many times this hashtag has occured in the past time window t*/
 	
 	public Float EG(); // Expected Gain
 	
 	public Float HE(NewsArticle a, Hashtag h); // Hashtag in hEadLine
 	
-	public Float UR(NewsArticle a, Hashtag h, Time LAMBDA ); // Unique user Ratio
+	public Float UR(NewsArticle a, Hashtag h, Date LAMBDA ); // Unique user Ratio
 	
-	public Float UC(NewsArticle a, Hashtag h, Time LAMBDA ); // User Credibility
-
-/*
-	public void loadNews();
-
-	public void loadNews(Date date);
-
-	public void loadNews(Integer count);
-
-	public void loadNews(Date date, Integer count);
-
-	public OutputStream getStream();
-
-	public NewsArticle getNextNews();
-*/
+	public Float UC(NewsArticle a, Hashtag h, Date LAMBDA ); // User Credibility
 
 }
