df86d56bed894b4153a506c9b889bcd347e2ba49
mrmotallebi <m.r.motallebi@gmail.com>
2016-12-09 20:26:52 +0900

    Added methods to HashtagFinder

--- src/main/java/me/motallebi/hashtagger/HashtagFinder.java
+++ src/main/java/me/motallebi/hashtagger/HashtagFinder.java
@@ -7,26 +7,67 @@ import twitter4j.Status;
  *
  */
 public interface HashtagFinder {
-	
+
 	/**
 	 * Get Tweets having a Hashtag
+	 * 
 	 * @param hashtag
 	 * @return List of Tweets
 	 */
 	public Status[] getStatusWithHT(String hashtag);
-	
+
 	/**
 	 * Get Tweets having a word
+	 * 
 	 * @param word
 	 * @return List of Tweets
 	 */
 	public Status[] getStatusWithWord(String word);
-	
+
 	/**
 	 * Get Hashtags for Tweets having a word
+	 * 
 	 * @param word
 	 * @return List of Hashtags
 	 */
 	public String[] getHTWithWord(String word);
+
+	/**
+	 * Get hashtags for tweets having both words
+	 * 
+	 * @param word1
+	 * @param word2
+	 * @return List of Hashtags
+	 */
+	public String[] getHTWithWord(String word1, String word2);
+
+	/**
+	 * Get all hashtags discovered
+	 * 
+	 * @return array of hashtags, preferably unique set
+	 */
+	public String[] getAllHashtags();
+
+	/**
+	 * Number of tweets mentioning this hashtag
+	 * 
+	 * @param hashtag
+	 * @return
+	 */
+	public int getTweetCountForHashtag(String hashtag);
+
+	/**
+	 * Get max number of tweets mentioning some hashtag
+	 * 
+	 * @return
+	 */
+	public int getMaxTweetsForHashtags();
+
+	/**
+	 * Get min number of tweets mentioning some hashtag
+	 * 
+	 * @return
+	 */
+	public int getMinTweetsForHashtags();
 
 }
--- src/main/java/me/motallebi/hashtagger/SimpleHashtagFinder.java
+++ src/main/java/me/motallebi/hashtagger/SimpleHashtagFinder.java
@@ -4,9 +4,13 @@
 package me.motallebi.hashtagger;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import twitter4j.HashtagEntity;
 import twitter4j.Status;
@@ -19,22 +23,18 @@ public class SimpleHashtagFinder implements HashtagFinder {
 
 	protected final List<Status> statusList;
 	protected final Map<String, List<Status>> invertedIndex;
+	protected final Map<String, List<Status>> htInvertedIndex;
 
 	public SimpleHashtagFinder(List<Status> statusList) {
 		this.statusList = statusList;
 		// Assume each two tweets has one unique word
 		this.invertedIndex = new HashMap<String, List<Status>>(
 				statusList.size() / 2);
+		this.htInvertedIndex = new HashMap<String, List<Status>>(
+				statusList.size() / 10);
 		for (Status status : this.statusList) {
 			String text = status.getText();
 			String[] mainWords = Util.getMainWords(text);
-
-			HashtagEntity[] hashtags = status.getHashtagEntities();
-			String[] hashtagWords = new String[hashtags.length];
-			int counter = 0;
-			for (HashtagEntity hashtag : hashtags) {
-				hashtagWords[counter++] = hashtag.getText().toLowerCase();
-			}
 			// Add tweet for each word
 			for (String word : mainWords) {
 				if (!invertedIndex.containsKey(word)) {
@@ -47,13 +47,14 @@ public class SimpleHashtagFinder implements HashtagFinder {
 				}
 			}
 			// Add tweet for hashtags
-			for (String word : hashtagWords) {
-				if (!invertedIndex.containsKey(word)) {
+			for (HashtagEntity hashtag : status.getHashtagEntities()) {
+				String word = hashtag.getText().toLowerCase();
+				if (!htInvertedIndex.containsKey(word)) {
 					List<Status> statuses = new ArrayList<Status>();
-					invertedIndex.put(word, statuses);
+					htInvertedIndex.put(word, statuses);
 					statuses.add(status);
 				} else {
-					List<Status> statuses = invertedIndex.get(word);
+					List<Status> statuses = htInvertedIndex.get(word);
 					statuses.add(status);
 				}
 			}
@@ -68,11 +69,13 @@ public class SimpleHashtagFinder implements HashtagFinder {
 	 */
 	@Override
 	public Status[] getStatusWithHT(String hashtag) {
-		if (this.invertedIndex.get(hashtag)==null){
-			Status d[] =  new Status[0];
-			return  d;
+		if (this.htInvertedIndex.get(hashtag) == null) {
+			Status d[] = new Status[0];
+			return d;
 		}
-		return this.invertedIndex.get(hashtag).toArray(new Status[0]);
+		// .toArray(new Status[0]) automatically recreates an array to match the
+		// size
+		return this.htInvertedIndex.get(hashtag).toArray(new Status[0]);
 	}
 
 	/*
@@ -83,7 +86,7 @@ public class SimpleHashtagFinder implements HashtagFinder {
 	 */
 	@Override
 	public Status[] getStatusWithWord(String word) {
-		return (Status[]) this.invertedIndex.get(word).toArray();
+		return this.invertedIndex.get(word).toArray(new Status[0]);
 	}
 
 	/*
@@ -94,8 +97,100 @@ public class SimpleHashtagFinder implements HashtagFinder {
 	 */
 	@Override
 	public String[] getHTWithWord(String word) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!this.invertedIndex.containsKey(word))
+			return new String[0];
+		Set<String> hashtagSet = new HashSet<>();
+		for (Status status : this.invertedIndex.get(word)) {
+			for (HashtagEntity hashtag : status.getHashtagEntities()) {
+				hashtagSet.add(hashtag.getText().toLowerCase());
+			}
+		}
+		return hashtagSet.toArray(new String[0]);
+	}
+
+	/* (non-Javadoc)
+	 * @see me.motallebi.hashtagger.HashtagFinder#getHTWithWord(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String[] getHTWithWord(String word1, String word2) {
+		if (word2 == null || word2.isEmpty())
+			return getHTWithWord(word1);
+		if (!this.invertedIndex.containsKey(word1)
+				&& !this.invertedIndex.containsKey(word2))
+			return new String[0];
+		Set<String> hashtagSet1 = new HashSet<>();
+		for (Status status : this.invertedIndex.get(word1)) {
+			for (HashtagEntity hashtag : status.getHashtagEntities()) {
+				hashtagSet1.add(hashtag.getText().toLowerCase());
+			}
+		}
+		Set<String> hashtagSet2 = new HashSet<>();
+		for (Status status : this.invertedIndex.get(word2)) {
+			for (HashtagEntity hashtag : status.getHashtagEntities()) {
+				hashtagSet2.add(hashtag.getText().toLowerCase());
+			}
+		}
+		hashtagSet1.retainAll(hashtagSet2);
+		return hashtagSet1.toArray(new String[0]);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see me.motallebi.hashtagger.HashtagFinder#getAllHashtags()
+	 */
+	@Override
+	public String[] getAllHashtags() {
+		Set<String> hashtagSet = this.htInvertedIndex.keySet();
+		return hashtagSet.toArray(new String[0]);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see me.motallebi.hashtagger.HashtagFinder#getMaxTweetsForHashtags()
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public int getMaxTweetsForHashtags() {
+		return Collections.max(this.htInvertedIndex.values(),
+				new Comparator<List>() {
+					@Override
+					public int compare(List o1, List o2) {
+						return o1.size() - o2.size();
+					}
+				}).size();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see me.motallebi.hashtagger.HashtagFinder#getMinTweetsForHashtags()
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public int getMinTweetsForHashtags() {
+		return Collections.min(this.htInvertedIndex.values(),
+				new Comparator<List>() {
+					@Override
+					public int compare(List o1, List o2) {
+						return o1.size() - o2.size();
+					}
+				}).size();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * me.motallebi.hashtagger.HashtagFinder#getTweetCountForHashtag(java.lang
+	 * .String)
+	 */
+	@Override
+	public int getTweetCountForHashtag(String hashtag) {
+		if (!this.htInvertedIndex.containsKey(hashtag))
+			return 0;
+		return this.htInvertedIndex.get(hashtag).size();
 	}
 
 }
